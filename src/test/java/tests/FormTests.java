839fa9a64d675599de048ecb7b4990e11a760642
RubiIren <rubinshteiniv@gmail.com>
2022-01-25 11:05:09 +0300

    Fix
    Удаление ресайзера, добавление даты рождения, хобби и изображения

--- src/test/java/tests/FormTests.java
+++ src/test/java/tests/FormTests.java
@@ -4,37 +4,47 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FormTests {
-        @BeforeAll
-        static void setup() {
-            Configuration.startMaximized = true;
-        }
-
-        @Test
-        void successfulSubmitFormTest() {
-
-            open("https://demoqa.com/automation-practice-form");
-
-            $("#firstName").setValue("Name");
-            $("#lastName").setValue("Surname");
-            $("#userEmail").setValue("podic58232@itwbuy.com");
-            $("[for=gender-radio-2").click();
-            $("#userNumber").setValue("8123456789");
-            $("[id=state]").click();
-            $("[id=react-select-3-option-0]").click();
-            $("[id=city]").click();
-            $("[id=react-select-4-option-0]").click();
-            $("#submit").click();
-
-            $(".table-responsive").shouldHave(text("Name Surname"),
-                    text("podic58232@itwbuy.com"),text("Female"),
-                    text("8123456789"), text("NCR Delhi"));
-
-        }
+    @BeforeAll
+    static void setup() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = false;
     }
+
+    @Test
+    void successfulSubmitFormTest() {
+
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue("Ira");
+        $("#lastName").setValue("Rubi");
+        $("#userEmail").setValue("podic58232@itwbuy.com");
+        $("[for=gender-radio-2").click();
+        $("#userNumber").setValue("8123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__day--022").click();
+        $("[for=hobbies-checkbox-1").click();
+        $("[for=hobbies-checkbox-3").click();
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
+        $("[id=state]").click();
+        $("[id=react-select-3-option-0]").click();
+        $("[id=city]").click();
+        $("[id=react-select-4-option-0]").click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("Ira Rubi"),
+                text("podic58232@itwbuy.com"), text("Female"),
+                text("8123456789"), text("22 June,1993"), text("Sports, Music"), text("cat.jpg"), text("NCR Delhi"));
+
+    }
+}
 
